02cad7261273d1b63900a3c2c1b8fbfde8eb2d00
HunorMarton <bor.hunor@gmail.com>
2020-10-16 17:57:34 +0200

    Hello world

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="Card">
+    <h3>Rick & Morty</h3>
+    <p>Animation</p>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.Card {
+  background-color: #ffffff;
+  border-radius: 10px;
+  box-shadow: 0 2px 2px 0 rgba(0,0,0,0.16);
+  display: inline-block;
+  margin: 20px;
+  padding: 5px 20px;
+  width: 200px;
 }
 </style>
