329af409b1db7d47468c97fde4b833cf8f53b5e3
HunorMarton <bor.hunor@gmail.com>
2020-10-16 19:36:23 +0200

    Input

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import NewCard from './NewCard.vue'
 import './index.css'
 
-createApp(App).mount('#app')
+createApp(NewCard).mount('#app')
